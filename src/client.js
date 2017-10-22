7aa5ae46754fef66c9937a45be49a02cf85c130b
Lucien Greathouse <me@lpghatguy.com>
2017-10-22 03:16:32 -0700

    Clean up client

--- src/client.js
+++ src/client.js
@@ -5,8 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.hydrate(
+const page = (
 	<BrowserRouter>
 		<App />
-	</BrowserRouter>,
-document.getElementById("root"));
+	</BrowserRouter>
+);
+
+ReactDOM.hydrate(page, document.getElementById("root"));
