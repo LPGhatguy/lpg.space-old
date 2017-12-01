26fdd365065434e948c4509b807029791e493b38
Lucien Greathouse <me@lpghatguy.com>
2017-11-30 21:42:51 -0800

    Blog updates

--- src/components/posts/new-site.js
+++ src/components/posts/new-site.js
@@ -23,8 +23,26 @@ export default {
 			</P>
 
 			<P>
-				I'm impressed with the server-side performance of React 16, and
-				having zero-latency page transitions is rad.
+				I had been exceedingly frustrated waiting for GitHub Pages to
+				support TLS for custom domains. Having a nice CDN with very
+				little setup is nice, but HTTPS is becoming more and more of a
+				hard requirement.
+			</P>
+
+			<P>
+				All-in-all, I'm impressed with the server-side performance of
+				React 16, and having zero-latency page transitions is rad.
+			</P>
+
+			<P>
+				The development experience using React and my own templates is
+				also significantly better than Jekyll. I gain hot-reloading and
+				a lot of flexibility in how I construct my pages.
+			</P>
+
+			<P>
+				The price I pay for this is much more setup and maintenance, as
+				well as hosting costs. I'm confident; we'll see if it pays off.
 			</P>
 		</Wysiwyg>
 	),
